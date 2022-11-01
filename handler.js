c0eaf266b8eb57d22bfe9df8aa91666da4d120d9
Celso Taliatelli <celsotaliatelli@hotmail.com>
2022-10-31 22:07:36 -0300

    add config criação tabela DynamoDB

--- handler.js
+++ handler.js
@@ -6,23 +6,35 @@ const pacientes = [
   { id: 2, nome: "Joao", idade: 30 },
   { id: 3, nome: "Jose", idade: 45 }
 
-]
+];
+
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+const params = {
+  TableName: 'PACIENTES'
+};
 
 module.exports.listarPacientes = async (event) => {
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-       pacientes
-      },
-      null,
-      2
-    ),
-  };
+  try {
+    let data = await dynamoDb.scan(params).promise();
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    }
+  } catch (err) {
+    console.log("Error", err);
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception", 
+        message: err.message ? err.message : "Unknown error",
+      })
+    }
+  }
+  
 };
 
 module.exports.obterPaciente = async (event) => {
